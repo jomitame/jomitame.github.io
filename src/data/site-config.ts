6dde0c656c807e331174bc5c55730c8427c91fb7
jomitame <jomitame@gmail.com>
2025-02-03 16:36:28 -0500

    Update titles of info

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,7 +39,7 @@ export type SiteConfig = {
 
 const siteConfig: SiteConfig = {
     title: 'Jose Miguel Tapias Mejia',
-    subtitle: 'Backend Developer',
+    subtitle: 'Python Developer',
     description: 'Personal portfolio and blog',
     image: {
         src: '/dante-preview.jpg',
@@ -92,7 +92,7 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Jose Miguel Tapias Mejia',
+        title: 'About Me',
         text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
